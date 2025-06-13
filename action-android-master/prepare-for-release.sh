627032d3ae084fae9dbfb8d6a313b8d1184e390a
mimaling <1606109137@qq.com>
2025-06-13 23:22:17 +0800

    Add files via upload

---
+++ action-android-master/prepare-for-release.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -ex
+
+for i in emulator-run-cmd install-sdk; do
+  (cd $i && docker run -it -v $(pwd):/opt/app -w /opt/app node:12 bash -c 'npm install && npm run build && npm prune --production' && git add -f node_modules lib)
+done
